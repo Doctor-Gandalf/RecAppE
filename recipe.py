7ffe8538019a35d6a1c91213952232552a61e4d7
Kellan Childers <Kellan Childers@gmail.com>
2015-08-02 16:09:23 -0400

    Added simple demonstration and test suite.

--- recipe.py
+++ recipe.py
@@ -9,7 +9,8 @@ class Recipe:
         try:
             current = self.ingredients[name]
             if current[1] == qualifier:
-                current[0] += quantity
+                new_quantity = current[0] + quantity
+                self.ingredients[name] = (new_quantity, qualifier)
             else:
                 raise ValueError("Attempting to add two different quantities failed.")
         except KeyError:
@@ -21,3 +22,23 @@ class Recipe:
             return self.ingredients[name]
         else:
             raise ValueError("No ingredient by that name.")
+
+    def show_ingredient(self, name):
+        return str(self.get_ingredient_quantity(name)[0]) + ' ' + \
+            str(self.get_ingredient_quantity(name)[1] + ' ' + str(name))
+
+if __name__ == "__main__":
+    print("Demonstrating recipe.py\n")
+    recipe = Recipe()
+    print("Adding an ingredient to recipe, then showing.")
+    recipe.add_ingredient("onions", 3, "whole")
+    print("There are {}.\n".format(recipe.show_ingredient("onions")))
+    print("Adding more ingredients to the recipe, then showing.\n")
+    print("Adding shallots.")
+    recipe.add_ingredient("shallots", 5, "chopped")
+    print("There are {0} and {1}.\n".format(recipe.show_ingredient("onions"), recipe.show_ingredient("shallots")))
+    print("Attempting to add diced onions.")
+    try:
+        recipe.add_ingredient("onions", 3, "diced")
+    except ValueError as e:
+        print(e.args[0])
