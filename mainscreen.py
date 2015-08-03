83baef4c201f5777ae7613ecef2b4155c17f54a2
Kellan Childers <Kellan Childers@gmail.com>
2015-08-03 16:28:13 -0400

    Centered display window.

--- mainscreen.py
+++ mainscreen.py
@@ -1,14 +1,17 @@
 __author__ = 'Kellan Childers'
 
 import curses
+import util
 from recipe import Recipe
 
 
 class MainScreen:
     def __init__(self, console_height, console_width):
         self._list_height, self._list_width = console_height-2, console_width-2
+        display_start_y, display_start_x = util.center_start(console_height, console_width,
+                                                             self._list_height, self._list_width)
         self._shopping_list = Recipe()
-        self._list_display = curses.newwin(self._list_height, self._list_width)
+        self._list_display = curses.newwin(self._list_height, self._list_width, display_start_y, display_start_x)
 
     def add_recipe(self, filename):
         try:
